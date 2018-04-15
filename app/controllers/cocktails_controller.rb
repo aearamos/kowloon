e29c915eae4c635326cdfd73b7a841655751e4dd
Arthur Ramos <ramos.oceano@gmail.com>
2018-04-16 00:21:39 +0200

    controllers_all_first_setup

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,22 +1,42 @@
 class CocktailsController < ApplicationController
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.save
   end
 
   def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(params[:cocktail])
   end
 
   def edit
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
   end
 
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:title, :description, :ingredients, :price, :photo)
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,42 @@
 class EventsController < ApplicationController
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.find(params[:id])
+    @event.save
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(params[:event])
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
   end
 
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :initime, :endtime, :photo)
   end
 end
