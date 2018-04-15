dd7200962e454f0863bf476c97d87f15d0fcc453
Arthur Ramos <ramos.oceano@gmail.com>
2018-04-15 23:26:05 +0200

    validations_associations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,5 @@
 class Cocktail < ApplicationRecord
+	belongs_to :user
+	validates :description, presence: :true
+	validates :title, presence: :true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+	belongs_to :user
+	validates :description, presence: :true
+	validates :title, presence: :true
+	validates :initime, presence: :true
+	validates :endtime, presence: :true
+	validates :date, presence: :true
 end
