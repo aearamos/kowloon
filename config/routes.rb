0b6043b306be7e78e049b634a010e7b0ca8abfd2
Arthur Ramos <ramos.oceano@gmail.com>
2018-04-15 22:52:39 +0200

    controller_foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'foods/new'
+
+  get 'foods/create'
+
+  get 'foods/update'
+
+  get 'foods/edit'
+
+  get 'foods/destroy'
+
+  get 'foods/index'
+
+  get 'foods/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
